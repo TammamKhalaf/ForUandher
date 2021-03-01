d1c9002e115f519b2c8bab95060c768b078f9610
Tammam Khalaf <tammam-963@hotmail.com>
2021-03-02 01:12:00 +0400

    Local State vs App-wide State

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'file:///C:/Users/TAMMAM/foruandher/lib/providers/product.dart';
-import 'package:foruandher/widgets/product_item.dart';
 import 'package:foruandher/widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum filterOptions { Favorites, All }
 
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('For U & her'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (filterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == filterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Only Favorites'),
+                  value: filterOptions.Favorites),
+              PopupMenuItem(child: Text('Show All'), value: filterOptions.All),
+            ],
+          ),
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
-
-
