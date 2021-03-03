5418e821cdc015069dfe2e4871a5dfc51413c5e6
Tammam Khalaf <tammam-963@hotmail.com>
2021-03-03 19:08:12 +0400

    Adding a "Manage Products" Page

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:foruandher/widgets/app_drawer.dart';
 import 'package:foruandher/widgets/user_product_item.dart';
 import 'package:provider/provider.dart';
 
@@ -12,6 +13,7 @@ class UserProductsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
     return Scaffold(
+      drawer: AppDrawer(),
       appBar: AppBar(
         title: const Text('Your Products'),
         actions: [
@@ -26,9 +28,12 @@ class UserProductsScreen extends StatelessWidget {
         padding: EdgeInsets.all(8),
         child: ListView.builder(
             itemCount: productsData.items.length,
-            itemBuilder: (_, i) => UserProductItem(
-                title: productsData.items[i].title,
-                imageUrl: productsData.items[i].imageUrl)),
+            itemBuilder: (_, i) => Column(children: [
+                  UserProductItem(
+                      title: productsData.items[i].title,
+                      imageUrl: productsData.items[i].imageUrl),
+                  Divider()
+                ])),
       ),
     );
   }
