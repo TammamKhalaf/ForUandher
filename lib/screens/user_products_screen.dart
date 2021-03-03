c0735b2f6c9216a6079e0b17df294276793f3248
Tammam Khalaf <tammam-963@hotmail.com>
2021-03-03 18:27:54 +0400

    Adding a "Manage Products" Page

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:foruandher/widgets/user_product_item.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                //...
+              })
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+            itemCount: productsData.items.length,
+            itemBuilder: (_, i) => UserProductItem(
+                title: productsData.items[i].title,
+                imageUrl: productsData.items[i].imageUrl)),
+      ),
+    );
+  }
+}
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:foruandher/screens/orders_screen.dart';
+import 'package:foruandher/screens/user_products_screen.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -27,6 +28,15 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(OrdersScreen.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage products'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(UserProductsScreen.routeName);
             },
           ),
         ],
