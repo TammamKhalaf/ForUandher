e97677da9b53e9bdd95cf7b7a7bd4adc8caa21aa
Tammam Khalaf <tammam-963@hotmail.com>
2021-03-06 18:26:00 +0400

    Utilizing Optimistic Updating

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -139,7 +139,22 @@ class Products with ChangeNotifier {
   }
 
   void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+    final url =
+        'https://for-u-and-her-flutter-default-rtdb.firebaseio.com/products/$id.json';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+
+    http.delete(Uri.parse(url)).then((response) {
+      if(response.statusCode >= 400){
+        throw error;
+      }
+      existingProduct = null;
+    }).catchError((_) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+    });
+
+    _items.removeAt(existingProductIndex);
     notifyListeners();
   }
 }
