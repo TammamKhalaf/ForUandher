aa5c832b240bbe7bd02971377c8985c3dfd046f6
Tammam Khalaf <tammam-963@hotmail.com>
2021-03-08 17:35:25 +0400

    Working with Slivers

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,43 +1,41 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:foruandher/providers/products.dart';
 import 'package:provider/provider.dart';
 
+import '../providers/products.dart';
+
 class ProductDetailScreen extends StatelessWidget {
-  final String title;
-  final double price;
+  // final String title;
+  // final double price;
 
-  const ProductDetailScreen({this.title, this.price});
-
+  // ProductDetailScreen(this.title, this.price);
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String;
-    final loadedProduct =
-        Provider.of<Products>(context, listen: false).findById(productId);
+    final productId =
+        ModalRoute.of(context).settings.arguments as String; // is the id!
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [
+          children: <Widget>[
             Container(
+              height: 300,
               width: double.infinity,
-              child: Hero(
-                tag: productId,
-                child: Image.network(
-                  loadedProduct.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Text(
-              '\£ ${loadedProduct.price}',
+              '\$${loadedProduct.price}',
               style: TextStyle(
                 color: Colors.grey,
                 fontSize: 20,
@@ -54,7 +52,7 @@ class ProductDetailScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
                 softWrap: true,
               ),
-            ),
+            )
           ],
         ),
       ),
